4fe1b53dcd0cce84bb411b0544adb146b482db9b
Seungwoo-Yu <seungwooyu2000@gmail.com>
2023-07-05 17:52:56 +0900

    fix(common/utils): add ZodDateTime

--- packages/common/src/utils.ts
+++ packages/common/src/utils.ts
@@ -1,5 +1,15 @@
 import { Enum } from '@hexa/common/types.ts';
-import { ZodIssueOptionalMessage } from 'zod';
+import {
+  addIssueToContext,
+  ParseInput,
+  ParseReturnType,
+  ProcessedCreateParams,
+  ZodErrorMap, ZodIssueCode,
+  ZodIssueOptionalMessage, ZodParsedType,
+  ZodType,
+  ZodTypeDef,
+} from 'zod';
+import { DateTime } from 'luxon';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function isValidOfEnum<T extends readonly unknown[]>(enumList: T, value: any): value is Enum<T> {
@@ -17,14 +27,17 @@ export function unifyZodMessages(message: string) {
       return { message: issue.message ?? message };
     }
 
-    if (issue.code === 'invalid_type' &&
-      (issue.expected !== 'string' || [
+    if (issue.code === 'invalid_type') {
+      if (([
         'null',
         'undefined',
         'never',
         'unknown',
         'void',
       ].indexOf(issue.received) === -1)) {
+        return { message: issue.message ?? message };
+      }
+
       return { message: issue.message ?? message };
     }
 
@@ -32,6 +45,114 @@ export function unifyZodMessages(message: string) {
       message,
     };
   };
+}
+
+export class ZodDateTime extends ZodType<DateTime, ZodTypeDef, DateTime> {
+  constructor(
+    private readonly name = 'DateTime',
+    params: ProcessedCreateParams,
+  ) {
+    super(params);
+  }
+
+  public static create(
+    name = 'DateTime',
+    params?: ZodTypeDef | ((name: string) => ZodTypeDef),
+  ) {
+    return new ZodDateTime(name, ZodDateTime.processCreateParams(
+      typeof params === 'function' ? params(name) : params,
+    ));
+  }
+
+  /**
+   * Based on {@link https://github.com/colinhacks/zod/blob/3.18/src/types.ts#L109}
+   */
+  private static processCreateParams(params?: ZodTypeDef): ProcessedCreateParams {
+    if (!params) return {};
+    const {
+      errorMap,
+      description,
+    } = params;
+    if (errorMap) return { errorMap: errorMap, description };
+    const customMap: ZodErrorMap = (iss, ctx) => {
+      if (iss.code !== 'invalid_type') return { message: ctx.defaultError };
+      if (iss.received === 'object') return { message: 'Expected DateTime, received object but not Datetime' };
+      return { message: `Expected DateTime, received ${iss.received}` };
+    };
+    return { errorMap: customMap, description };
+  }
+
+  /**
+   * Based on {@link https://github.com/colinhacks/zod/blob/3.18/src/types.ts#L482}
+   */
+  public _parse(input: ParseInput): ParseReturnType<DateTime> {
+    const type = this._getTypeCustom(input);
+    const ctx = this._getOrReturnCtx(input);
+
+    if (type !== 'object') {
+      addIssueToContext(ctx, {
+        code: ZodIssueCode.invalid_type,
+        expected: ZodParsedType.object,
+        received: type,
+      });
+      return {
+        status: 'aborted',
+      };
+    }
+
+    if (!DateTime.isDateTime(input.data)) {
+      addIssueToContext(ctx, {
+        code: ZodIssueCode.custom,
+        params: {
+          code: 'INVALID_DATETIME_TYPE',
+        },
+      });
+      return {
+        status: 'aborted',
+      };
+    }
+
+    return {
+      status: 'valid',
+      value: input.data,
+    };
+  }
+
+  public _getTypeCustom(input: ParseInput): ZodParsedType {
+    if (typeof input.data === 'object') {
+      return ZodParsedType.object;
+    }
+    if (typeof input.data === 'string') {
+      return ZodParsedType.string;
+    }
+    if (typeof input.data === 'number') {
+      return ZodParsedType.number;
+    }
+    if (typeof input.data === 'bigint') {
+      return ZodParsedType.bigint;
+    }
+    if (typeof input.data === 'symbol') {
+      return ZodParsedType.symbol;
+    }
+    if (typeof input.data === 'undefined') {
+      return ZodParsedType.undefined;
+    }
+    if (typeof input.data === 'function') {
+      return ZodParsedType.function;
+    }
+
+    return ZodParsedType.unknown;
+  }
+
+  public isUTC() {
+    return this.refine(
+      date => date.zoneName === 'UTC',
+      {
+        message: 'Timezone of ' + this.name + ' is not UTC',
+        params: { name: this.name, code: 'INVALID_UTC_TIMEZONE' },
+      },
+    );
+  }
 }
 
 /**
