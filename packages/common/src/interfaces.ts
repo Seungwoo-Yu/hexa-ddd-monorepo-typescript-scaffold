16cfe653e8bc851967693d9c09f97d4fd35c4cbc
Seungwoo-Yu <seungwooyu2000@gmail.com>
2023-07-05 17:48:30 +0900

    fix(common/interfaces): change return type of validate

--- packages/common/src/interfaces.ts
+++ packages/common/src/interfaces.ts
@@ -9,13 +9,25 @@ export interface IFactory<T extends object> {
 }
 
 export interface Validatable {
-  validate(): boolean,
+  /**
+   * @param target something to check if it is valid or not
+   * @throws {MinLengthError} will be thrown when target is shorter than expected length
+   * @throws {MaxLengthError} will be thrown when target is longer than expected length
+   * @throws {MinValueError} will be thrown when target is less than expected value
+   * @throws {MaxValueError} will be thrown when target is greater than expected value
+   * @throws {MinElementLenError} will be thrown when target contains fewer elements than expected
+   * @throws {MaxElementLenError} will be thrown when target container more elements than expected
+   * @throws {InvalidEnumError} will be thrown when expected enum does not contain target
+   * @throws {UndefOrNullParamError} will be thrown when target is either undefined or null
+   */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  validate(target: any): void,
 }
 
 export interface Equality {
   /**
    * @param other something to check if this is equal to it
-   * @throws {UndefOrNullParamError} Will be thrown when other is either undefined or null
+   * @throws {UndefOrNullParamError} will be thrown when other is either undefined or null
    */
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   equals(other: any): boolean,
