1250c7e506b57d08d4787403f321c545b9f19233
Seungwoo-Yu <seungwooyu2000@gmail.com>
2023-07-11 10:12:40 +0900

    fix(store-domain/factories): check items for StoreFactory.create

--- packages/store-domain/src/domains/factories/store.factory.test.ts
+++ packages/store-domain/src/domains/factories/store.factory.test.ts
@@ -1,4 +1,4 @@
-import { StoreFactory } from '@hexa/store-domain/domains/factories/store.factory.ts';
+import { StoreFactory, StoreIdNotMatchedError } from '@hexa/store-domain/domains/factories/store.factory.ts';
 import { StoreAgg } from '@hexa/store-domain/domains/aggs/store.agg.ts';
 import { Store } from '@hexa/store-domain/domains/entities/store.entity.ts';
 import { IntegerUid } from '@hexa/store-domain/domains/vo/integer-uid.vo.ts';
@@ -14,7 +14,12 @@ import { Price } from '@hexa/store-domain/domains/vo/price.vo.ts';
 describe('store-domain factory test', () => {
   it('should be generated', async () => {
     const incrIntegerFactory = new IncrIntegerFactory();
-    const store = new Store(new IntegerUid(incrIntegerFactory.next()), new StoreName('name'), new StoreDesc('description'), UlidUid.create());
+    const store = new Store(
+      new IntegerUid(incrIntegerFactory.next()),
+      new StoreName('name'),
+      new StoreDesc('description'),
+      UlidUid.create(),
+    );
 
     const storeAgg = await StoreFactory.create(store, []);
 
@@ -24,7 +29,12 @@ describe('store-domain factory test', () => {
 
   it('should be generated with items', async () => {
     const incrIntegerFactory = new IncrIntegerFactory();
-    const store = new Store(new IntegerUid(incrIntegerFactory.next()), new StoreName('name 2'), new StoreDesc('description 2'), UlidUid.create());
+    const store = new Store(
+      new IntegerUid(incrIntegerFactory.next()),
+      new StoreName('name 2'),
+      new StoreDesc('description 2'),
+      UlidUid.create(),
+    );
     const items = [
       new Item(
         new IntegerUid(incrIntegerFactory.next()),
@@ -51,5 +61,35 @@ describe('store-domain factory test', () => {
     expect(storeAgg.items.length).toStrictEqual(2);
     expect(storeAgg.items[0].name.name).toStrictEqual(items[0].name.name);
     expect(storeAgg.items[1].name.name).toStrictEqual(items[1].name.name);
+  });
+
+  it('should not be generated with items ' +
+    'because some of them do not have matched store id with store', async () => {
+    const incrIntegerFactory = new IncrIntegerFactory();
+    const store = new Store(
+      new IntegerUid(incrIntegerFactory.next()),
+      new StoreName('name 2'),
+      new StoreDesc('description 2'),
+      UlidUid.create(),
+    );
+    const items = [
+      new Item(
+        new IntegerUid(incrIntegerFactory.next()),
+        new ItemName('item 1'),
+        new ItemDesc('this is item 1'),
+        new Price(10000),
+        store.uid,
+      ),
+      new Item(
+        new IntegerUid(incrIntegerFactory.next()),
+        new ItemName('item 2'),
+        new ItemDesc('this is item 2'),
+        new Price(15000),
+        new IntegerUid(9999), // This does not exist and will cause error
+      ),
+    ];
+
+    await expect(() => StoreFactory.create(store, items))
+      .toThrowError(new StoreIdNotMatchedError(store.uid, items[1].uid, items[1].storeUid));
   });
 });
--- packages/store-domain/src/domains/factories/store.factory.ts
+++ packages/store-domain/src/domains/factories/store.factory.ts
@@ -4,7 +4,17 @@ import { StoreAgg } from '@hexa/store-domain/domains/aggs/store.agg.ts';
 import { OrderedMap } from 'immutable';
 import { IFactory } from '@hexa/common/interfaces.ts';
 import { AssertStaticInterface } from '@hexa/common/decorators.ts';
-import { ReadOnlyProperty } from '@hexa/common/types.ts';
+import { PickType, ReadOnlyProperty } from '@hexa/common/types.ts';
+
+export class StoreIdNotMatchedError extends Error {
+  constructor(
+    storeId: PickType<Item, 'uid'>,
+    itemId: PickType<Item, 'uid'>,
+    invalidStoreId: PickType<Item, 'uid'>,
+  ) {
+    super('store ' + invalidStoreId.uid + ' found in item ' + itemId.uid + ' is not matched with store ' + storeId.uid);
+  }
+}
 
 @AssertStaticInterface<IFactory<StoreAgg>>()
 export class StoreFactory {
@@ -12,6 +22,12 @@ export class StoreFactory {
     store: ReadOnlyProperty<Store, 'uid'>,
     items: ReadOnlyProperty<Item, 'uid' | 'storeUid'>[],
   ) {
+
+    items.forEach(item => {
+      if (!store.uid.equals(item.storeUid)) {
+        throw new StoreIdNotMatchedError(store.uid, item.uid, item.storeUid);
+      }
+    });
 
     return new StoreAgg(
       store,
