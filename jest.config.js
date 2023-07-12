5253aab8e181bfef575d537709cf05c6c85fa2b6
Seungwoo-Yu <seungwooyu2000@gmail.com>
2023-07-12 12:57:59 +0900

    chore: update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,24 @@ module.exports = {
   transform: {
     '^.+\\.(t|j)sx?$': 'ts-jest',
   },
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+  ],
+  modulePathIgnorePatterns: [
+    '<rootDir>/dist/',
+  ],
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '\\.pnp\\.[^\\/]+$',
+    '<rootDir>/dist/',
+  ],
+  watchPathIgnorePatterns: [
+    '<rootDir>/dist/',
+  ],
   coveragePathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
     '<rootDir>/packages/.+/src/tests/mocks.ts',
   ],
 };
