5dbf529f9828ec9b561c189508f0560ba65f7f4f
Seungwoo-Yu <seungwooyu2000@gmail.com>
2023-07-12 13:23:18 +0900

    fix(user-domain/vo): add tests for Amount

---
+++ packages/user-domain/src/domains/vo/amount.vo.test.ts
@@ -0,0 +1,19 @@
+import { Amount } from './amount.vo';
+
+describe('amount vo test', () => {
+  it('should create successfully', () => {
+    const amount = new Amount(1);
+
+    expect(amount.amount).toStrictEqual(1);
+  });
+
+  it('should not create because it is less than', function () {
+    expect(() => new Amount(0)).toThrowError('composite validation error: 1 error(s) thrown.\n' +
+      'main error: amount must be greater than or equal to 1');
+  });
+
+  it('should not create because it cannot be negative value', function () {
+    expect(() => new Amount(-1)).toThrowError('composite validation error: 1 error(s) thrown.\n' +
+      'main error: amount must be greater than or equal to 1');
+  });
+});
