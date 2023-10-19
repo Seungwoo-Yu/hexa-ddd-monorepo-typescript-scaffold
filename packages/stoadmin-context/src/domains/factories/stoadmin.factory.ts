4aa18dc24689c5df4f8a5a5e407f96a5547036ff
Seungwoo-Yu <seungwooyu2000@gmail.com>
2023-10-19 15:25:16 +0900

    feat(stoadmin-context): move StoadminService.create
    into StoadminFactory using GeneratorOf interface

--- packages/stoadmin-context/src/domains/factories/stoadmin.factory.ts
+++ packages/stoadmin-context/src/domains/factories/stoadmin.factory.ts
@@ -1,9 +1,11 @@
 import { AssertStaticInterface } from '@hexa/common/decorators';
-import { FactoryOf } from '@hexa/common/interfaces';
+import { GeneratorOf } from '@hexa/common/interfaces';
 import { StoadminAgg } from '@hexa/stoadmin-context/domains/aggs/stoadmin.agg';
 import { Stoadmin } from '@hexa/stoadmin-context/domains/entities/stoadmin.entity';
 import { Store } from '@hexa/stoadmin-context/domains/entities/stoadmin-store.entity';
-import { PickType } from '@hexa/common/types';
+import { OmitFuncs, PickType } from '@hexa/common/types';
+import { UlidUid } from '@hexa/stoadmin-context/domains/vo/ulid-uid.vo';
+import { IStoadminCommand } from '@hexa/stoadmin-context/domains/repositories/commands/stoadmin.command';
 
 export class StoadminIdNotMatchedError extends Error {
   constructor(
@@ -16,7 +18,7 @@ export class StoadminIdNotMatchedError extends Error {
   }
 }
 
-@AssertStaticInterface<FactoryOf<StoadminAgg>>()
+@AssertStaticInterface<GeneratorOf<StoadminAgg>>()
 export class StoadminFactory {
   public static create(stoadmin: Stoadmin, stores: Store[] = []) {
     stores.forEach(store => {
@@ -26,5 +28,18 @@ export class StoadminFactory {
     });
 
     return new StoadminAgg(stoadmin, stores);
+  }
+
+  public static async generate(stoadminCommand: IStoadminCommand, _stoadmin: Omit<OmitFuncs<Stoadmin>, 'uid'>) {
+    const stoadmin = new Stoadmin(
+      UlidUid.create(),
+      _stoadmin.credential,
+      _stoadmin.name,
+    );
+    const stoadminAgg = StoadminFactory.create(stoadmin);
+
+    await stoadminCommand.create(stoadminAgg);
+
+    return stoadminAgg;
   }
 }
