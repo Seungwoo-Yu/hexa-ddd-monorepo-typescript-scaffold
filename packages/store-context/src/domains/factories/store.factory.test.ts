e4d465a30da21b7f61bc377d84474b789818ed70
Seungwoo-Yu <seungwooyu2000@gmail.com>
2023-10-19 15:46:36 +0900

    style(store-context): rename StoreService to StoreFactory

--- packages/store-context/src/domains/factories/store.factory.test.ts
+++ packages/store-context/src/domains/factories/store.factory.test.ts
@@ -1,5 +1,5 @@
 import { InMemoryStoreRepo } from '@hexa/store-context/tests/mocks';
-import { StoreService } from '@hexa/store-context/domains/services/store.service';
+import { StoreFactory } from '@hexa/store-context/domains/factories/store.factory';
 import { StoreDesc } from '@hexa/store-context/domains/vo/store-desc.vo';
 import { StoreName } from '@hexa/store-context/domains/vo/store-name.vo';
 import { UlidUid } from '@hexa/store-context/domains/vo/ulid-uid.vo';
@@ -8,9 +8,8 @@ import { ItemDesc } from '@hexa/store-context/domains/vo/item-desc.vo';
 import { Price } from '@hexa/store-context/domains/vo/price.vo';
 
 describe('store-domain service test', () => {
-  it('should create store with items', async () => {
+  it('should generate store with items', async () => {
     const repo = new InMemoryStoreRepo();
-    const service = new StoreService(repo, repo);
 
     const storeRaw = {
       name: new StoreName('test'),
@@ -24,7 +23,7 @@ describe('store-domain service test', () => {
         price: new Price(10000),
       },
     ];
-    const storeAgg = await service.create(storeRaw, itemRawList);
+    const storeAgg = await StoreFactory.generate(repo, repo, storeRaw, itemRawList);
 
     expect(storeAgg.store.name).toStrictEqual(storeRaw.name);
     expect(storeAgg.items[0].name).toStrictEqual(itemRawList[0].name);
