228b52ee48563847f7cee52fed67227826d9f1a3
Seungwoo-Yu <seungwooyu2000@gmail.com>
2023-11-13 16:31:07 +0900

    chore: use old decorator system

--- packages/common/src/decorators.ts
+++ packages/common/src/decorators.ts
@@ -1,4 +1,4 @@
 export function AssertStaticInterface<T>() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return <U extends { new(...args: any[]): any } & T>(constructor: U, _: ClassDecoratorContext) => constructor;
+  return <U extends { new(...args: any[]): any } & T>(constructor: U) => constructor;
 }
