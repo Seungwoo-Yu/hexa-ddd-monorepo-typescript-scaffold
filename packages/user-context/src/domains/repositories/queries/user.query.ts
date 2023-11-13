c84e53220b064422f8138d0b5aa9bb996a5f3174
Seungwoo-Yu <seungwooyu2000@gmail.com>
2023-11-13 16:36:52 +0900

    feat(user-context/repositories): add readById and existsById
    for IUserQuery

--- packages/user-context/src/domains/repositories/queries/user.query.ts
+++ packages/user-context/src/domains/repositories/queries/user.query.ts
@@ -3,5 +3,7 @@ import { PickType } from '@hexa/common/types';
 
 export interface IUserQuery {
   readByUid(uid: PickType<User, 'uid'>): Promise<User | undefined>,
+  readById(credential: PickType<User, 'credential'>): Promise<User | undefined>,
   exists(uid: PickType<User, 'uid'>): Promise<boolean>,
+  existsById(credential: PickType<User, 'credential'>): Promise<boolean>,
 }
