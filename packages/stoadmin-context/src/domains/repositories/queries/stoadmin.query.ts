a1a2ac14e45eba5b9ece4a0e0003ed002b1ea9a4
Seungwoo-Yu <seungwooyu2000@gmail.com>
2023-11-27 15:19:40 +0900

    feat(stoadmin-context/repositories): add readById
    and existsById for IStoadminQuery

--- packages/stoadmin-context/src/domains/repositories/queries/stoadmin.query.ts
+++ packages/stoadmin-context/src/domains/repositories/queries/stoadmin.query.ts
@@ -1,8 +1,11 @@
 import { PickType } from '@hexa/common/types';
 import { Stoadmin } from '@hexa/stoadmin-context/domains/entities/stoadmin.entity';
 import { StoadminAgg } from '@hexa/stoadmin-context/domains/aggs/stoadmin.agg';
+import { Credential } from '@hexa/stoadmin-context/domains/vo/credential.vo';
 
 export interface IStoadminQuery {
   readByUid(uid: PickType<Stoadmin, 'uid'>): Promise<StoadminAgg | undefined>,
+  readById(credentialId: PickType<Credential, 'id'>): Promise<StoadminAgg | undefined>,
   exists(uid: PickType<Stoadmin, 'uid'>): Promise<boolean>,
+  existsById(credentialId: PickType<Credential, 'id'>): Promise<boolean>,
 }
