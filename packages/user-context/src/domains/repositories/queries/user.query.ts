19eaf13c5898031e5ab4395b040fe208f7026ac1
Seungwoo-Yu <seungwooyu2000@gmail.com>
2023-11-27 15:11:42 +0900

    fix(user-context/repositories): change return type
    of readById and existsById for IUserQuery

--- packages/user-context/src/domains/repositories/queries/user.query.ts
+++ packages/user-context/src/domains/repositories/queries/user.query.ts
@@ -1,9 +1,10 @@
 import { User } from '@hexa/user-context/domains/entities/user.entity';
 import { PickType } from '@hexa/common/types';
+import { Credential } from '@hexa/user-context/domains/vo/credential.vo';
 
 export interface IUserQuery {
   readByUid(uid: PickType<User, 'uid'>): Promise<User | undefined>,
-  readById(credential: PickType<User, 'credential'>): Promise<User | undefined>,
+  readById(credentialId: PickType<Credential, 'id'>): Promise<User | undefined>,
   exists(uid: PickType<User, 'uid'>): Promise<boolean>,
-  existsById(credential: PickType<User, 'credential'>): Promise<boolean>,
+  existsById(credentialId: PickType<Credential, 'id'>): Promise<boolean>,
 }
